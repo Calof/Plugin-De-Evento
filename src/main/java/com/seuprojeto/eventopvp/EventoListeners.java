1a6966c188248328298bb6e531ad989023d14b32
Gabriel Valente <66322348+Calof@users.noreply.github.com>
2026-06-18 15:11:54 -0300

    Change player warp to teleport to original location

--- src/main/java/com/seuprojeto/eventopvp/EventoListeners.java
+++ src/main/java/com/seuprojeto/eventopvp/EventoListeners.java
@@ -82,7 +82,7 @@ public class EventoListeners implements Listener {
                 p.setFireTicks(0);
                 p.getActivePotionEffects().forEach(effect -> p.removePotionEffect(effect.getType()));
 
-                p.warp("Lobby");
+                p.teleport(locOriginal);
 
                 plugin.getJogadoresConfig().set(uuidStr, null);
                 plugin.saveJogadoresConfig();
